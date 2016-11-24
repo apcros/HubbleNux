43a39b951f292d6c05383b16b274befa75e91163
Apcros <apcros.mail@gmail.com>
2016-11-24 17:40:41 +0000

    idiotic issue solved

--- lib/Hubble/Engine.pm
+++ lib/Hubble/Engine.pm
@@ -6,8 +6,11 @@ use Hubble::Api;
 
 use Data::Dumper;
 
-
-has generic_stats => (is => 'ro', lazy => 1, builder => '_build_generic_stats');
+has generic_stats => (
+	is => 'ro',
+	lazy => 1,
+	builder => '_build_generic_stats'
+);
 has wait_time => (is => 'ro');
 
 sub _build_generic_stats {
@@ -15,9 +18,8 @@ sub _build_generic_stats {
 	return Hubble::Stats::Generic->new();
 }
 
-sub get_stats_hash {
+sub get_data_hash {
 	my ($self) = @_;
-	print Dumper($self);
 	return {
 		cpu_usage => $self->generic_stats->get_cpu_percent()
 	}
@@ -26,7 +28,7 @@ sub get_stats_hash {
 sub run {
 	my ($self) = @_;
 	while (1) {
-		my $stats_hash = get_stats_hash();
+		my $stats_hash = $self->get_data_hash();
 		print Dumper($stats_hash);
 		sleep $self->wait_time;
 	}
